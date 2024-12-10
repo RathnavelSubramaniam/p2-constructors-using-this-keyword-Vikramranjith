cd614189c16e4782ed5dd55d699a5e07fd22f67f
Vikramranjith <vikramranjith2006@gmail.com>
2024-12-10 09:24:11 +0000

    Commit 2

--- Practical2.java
+++ Practical2.java
@@ -1,28 +1,20 @@
 class Product {
-    int productId; // Corrected variable name to productId
+    int productId; 
     String productName;
     double price;
-    static int count = 0; // Initialize the static count variable
-
-    // Default constructor
+    static int count = 0; 
     Product() {
         productName = null;
     }
-
-    // Parameterized constructor
     Product(int productId, String productName, double price) {
         this.productId = productId;
         this.productName = productName;
         this.price = price;
-        count++; // Increment the static count variable when a new product is created
+        count++;
     }
-
-    // Method to calculate discount
     double discount(double price, double discountPercentage) {
         return ((100 - discountPercentage) / 100) * price;
     }
-
-    // Method to display product info
     void displayProductInfo() {
         System.out.println("Product Id: " + productId);
         System.out.println("Product Name: " + productName);
@@ -32,24 +24,15 @@ class Product {
 
 public class Practical2 {
     public static void main(String[] args) {
-        // Creating instances of Product class
         Product product1 = new Product(101, "Laptop", 999.99);
         Product product2 = new Product(202, "SmartPhone", 499.95);
         Product product3 = new Product(303, "Tablet", 299.50);
-        
-        // Create a temporary product instance for calculating discount
         Product calcDiscount = new Product();
-
-        // Display product information
         product1.displayProductInfo();
         product2.displayProductInfo();
         product3.displayProductInfo();
-
-        // Calculate and display discounted price for product1
-        double DiscountedPrice = calcDiscount.discount(product1.price, 10); // Applying 10% discount
+        double DiscountedPrice = calcDiscount.discount(product1.price, 10); 
         System.out.println("Discounted Price: " + DiscountedPrice);
-
-        // Display total number of products created (static count)
         System.out.println("Total products created: " + Product.count);
     }
 }
